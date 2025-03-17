a4f497c8122f4c745c97cbcb3763b93604597a9f
steve macharia <40029452+stevemacharia@users.noreply.github.com>
2025-03-17 10:17:12 +0300

    Start of Client Sidebar

--- app/client/dashboard/layout.tsx
+++ app/client/dashboard/layout.tsx
@@ -2,9 +2,10 @@ import SecondaryLayout from '@/components/SecondaryLayout';
 
 export default function MainLayoutWrapper({ children }: { children: React.ReactNode }) {
     return    ( 
-
+                
         <div className="main-layout">
                 <SecondaryLayout>
+                    
                     {children}
                 </SecondaryLayout>;
 
---
+++ components/ClientSideBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ClientSideBar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ClientSideBar
